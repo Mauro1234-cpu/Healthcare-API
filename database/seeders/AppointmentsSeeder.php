ada47dab92d3a00ebe06a264a80347bb4007356b
Mauro Aires <mauroaires@MacBook-Air-de-Mauro.local>
2025-10-01 16:48:49 -0300

    feat: create appintments migration, seeder and model

---
+++ database/seeders/AppointmentsSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Lightit\Shared\App\Appointments;
+
+class AppointmentsSeeder extends Seeder
+{
+    public function run(): void
+    {
+
+    }
+}
---
+++ src/Shared/App/Appointments.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lightit\Shared\App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Appointments extends Model
+{
+}
