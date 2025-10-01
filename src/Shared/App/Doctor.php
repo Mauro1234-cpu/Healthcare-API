bad64699a5fff58522ee562d55fd8345de19eddb
Mauro Aires <mauroaires@MacBook-Air-de-Mauro.local>
2025-10-01 16:51:34 -0300

    feat: create doctors migration, seeder and model

---
+++ src/Shared/App/Doctor.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lightit\Shared\App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+/**
+ * @property int                          $id
+ * @property string                       $name
+ * @property \Carbon\CarbonImmutable|null $created_at
+ * @property \Carbon\CarbonImmutable|null $updated_at
+ *
+ * @method static \Database\Factories\DoctorFactory                    factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Doctor newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Doctor newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Doctor query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Doctor whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Doctor whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Doctor whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Doctor whereUpdatedAt($value)
+ *
+ * @mixin \Eloquent
+ */
+class Doctor extends Model
+{
+    use HasFactory;
+
+    public function clinics(): BelongsToMany
+    {
+        return $this->belongsToMany(Clinics::class);
+    }
+}
