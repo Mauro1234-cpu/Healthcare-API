e9eb2126aeec90716366ef7e9ba7095fdb357a53
Mauro Aires <mauroaires@MacBook-Air-de-Mauro.local>
2025-10-21 15:04:09 -0300

    feat: Correct controllers, requests and models

--- src/Clinics/App/Controllers/ListClinicController.php
+++ src/Clinics/App/Controllers/ListClinicController.php
@@ -7,14 +7,12 @@ namespace Lightit\Clinics\App\Controllers;
 use Illuminate\Http\JsonResponse;
 use Lightit\Clinics\App\Resources\ClinicResource;
 use Lightit\Clinics\Domain\Actions\ListClinicAction;
-use Lightit\Clinics\Domain\Models\Clinic;
 
 final class ListClinicController
 {
     public function __invoke(
-        ListClinicAction $action
-    ): JsonResponse
-    {
+        ListClinicAction $action,
+    ): JsonResponse {
         $clinic = $action->execute();
 
         return ClinicResource::collection($clinic)
--- src/Users/Domain/Models/User.php
+++ src/Users/Domain/Models/User.php
@@ -12,7 +12,6 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Lightit\Appointments\Domain\Models\Appointment;
 
-
 /**
  * Domain\Users\Models\User
  *
@@ -28,6 +27,7 @@ use Lightit\Appointments\Domain\Models\Appointment;
  * @property-read int|null $notifications_count
  * @property-read \Illuminate\Database\Eloquent\Collection<int, \Laravel\Sanctum\PersonalAccessToken> $tokens
  * @property-read int|null $tokens_count
+ *
  * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|User query()
@@ -39,6 +39,7 @@ use Lightit\Appointments\Domain\Models\Appointment;
  * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ *
  * @mixin \Eloquent
  */
 class User extends Authenticatable
