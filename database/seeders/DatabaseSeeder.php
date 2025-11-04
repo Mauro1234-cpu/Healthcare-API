98ca4c6be5b9af9dc8dee6e705ce6eaa5a084c7a
Mauro Aires <mauroaires@MacBook-Air-de-Mauro.local>
2025-11-04 15:14:46 -0300

    fix: Correct the name Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,9 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             DoctorSeeder::class,
-            ClinicsSeeder::class,
+            ClinicSeeder::class,
             UserSeeder::class,
-            AppointmentsSeeder::class,
+            AppointmentSeeder::class,
         ]);
     }
 }
