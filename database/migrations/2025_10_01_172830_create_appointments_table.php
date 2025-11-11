d4c6920a36e0d889cf05660012f888d254b9cf6c
Mauro Aires <mauroaires@MacBook-Air-de-Mauro.local>
2025-11-11 17:15:29 -0300

    feature: Add Soft Delete to Appointment Model

--- database/migrations/2025_10_01_172830_create_appointments_table.php
+++ database/migrations/2025_10_01_172830_create_appointments_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->datetime('start_time');
             $table->datetime('end_time');
-            $table->timestamps();
+            $table->softDeletes();
         });
     }
 
