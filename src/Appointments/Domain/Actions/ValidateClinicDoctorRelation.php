7f242d64fca50de4f6358702c269b3b9647be2f8
Mauro Aires <mauroaires@MacBook-Air-de-Mauro.local>
2025-11-12 17:07:06 -0300

    refactor: Change the querie for a Eloquent Query

--- src/Appointments/Domain/Actions/ValidateClinicDoctorRelation.php
+++ src/Appointments/Domain/Actions/ValidateClinicDoctorRelation.php
@@ -4,16 +4,15 @@ declare(strict_types=1);
 
 namespace Lightit\Appointments\Domain\Actions;
 
-use Illuminate\Support\Facades\DB;
 use Lightit\Appointments\Domain\DataTransferObjects\AppointmentDto;
+use Lightit\Doctors\Domain\Models\Doctor;
 
 class ValidateClinicDoctorRelation
 {
     public function execute(AppointmentDto $appointmentDto): bool
     {
-        return DB::table('clinic_doctor')
-            ->where('doctor_id', $appointmentDto->doctor_id)
-            ->where('clinic_id', $appointmentDto->clinic_id)
+        return Doctor::whereId($appointmentDto->doctor_id)
+            ->whereHas('clinics', fn ($q) => $q->whereClinicId($appointmentDto->clinic_id))
             ->exists();
     }
 }
