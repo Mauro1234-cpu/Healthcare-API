98310566e13ebd04934e0b846c39199ebb63b24b
Mauro Aires <mauroaires@MacBook-Air-de-Mauro.local>
2025-11-12 15:36:16 -0300

    refactor: Improve flexibility of Rleation exception

--- src/Appointments/App/Exceptions/RelationException.php
+++ src/Appointments/App/Exceptions/RelationException.php
@@ -5,17 +5,16 @@ declare(strict_types=1);
 namespace Lightit\Appointments\App\Exceptions;
 
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Response;
 use Lightit\Shared\App\Exceptions\Http\HttpException;
 
 class RelationException extends HttpException
 {
-    public function render(): Response
-    {
-        $status = 409;
-        $message = 'This doctor does not work at this clinic';
+    protected int $status = JsonResponse::HTTP_CONFLICT;
 
-        return response(['status' => $status, 'message' => $message])
-        ->setStatusCode(JsonResponse::HTTP_CONFLICT);
+    protected string $errorCode = 'APPOINTMENT_OVERLAP';
+
+    public function __construct()
+    {
+        parent::__construct('This doctor does not work at this clinic');
     }
 }
