c7b78b2709fec4ed5f1f5a918fbda25b70f79831
Mauro Aires <mauroaires@MacBook-Air-de-Mauro.local>
2025-11-12 16:19:24 -0300

    fix: Correct the name of the actions and its alias

--- src/Appointments/App/Controllers/UpdateAppointmentController.php
+++ src/Appointments/App/Controllers/UpdateAppointmentController.php
@@ -7,7 +7,7 @@ namespace Lightit\Appointments\App\Controllers;
 use Illuminate\Http\JsonResponse;
 use Lightit\Appointments\App\Requests\UpsertAppointmentRequest;
 use Lightit\Appointments\App\Resources\AppointmentResource;
-use Lightit\Appointments\Domain\Actions\UpdateAppointmentAction;
+use Lightit\Appointments\Domain\Actions\UpsertAppointmentAction;
 use Lightit\Appointments\Domain\Models\Appointment;
 
 final class UpdateAppointmentController
@@ -15,9 +15,9 @@ final class UpdateAppointmentController
     public function __invoke(
         Appointment $appointment,
         UpsertAppointmentRequest $request,
-        UpdateAppointmentAction $updateAppointmentAction,
+        UpsertAppointmentAction $upsertAppointmentAction,
     ): JsonResponse {
-        $appointment = $updateAppointmentAction->execute($appointment, $request->toDto());
+        $appointment = $upsertAppointmentAction->execute($request->toDto(), $appointment);
 
         return AppointmentResource::make($appointment)
             ->response();
