648d07d64288d447d59f26809be9948ec5dc6e38
Mauro Aires <mauroaires@MacBook-Air-de-Mauro.local>
2025-12-01 15:02:54 -0300

    test: Add clinic-doctor relation validation test

---
+++ tests/Feature/Appointments/RelationClinicAndDoctorTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Appointments;
+
+describe('clinic-doctor relation validation', function (): void {
+    it('throw an exception if the doctor does not work at the selected clinic', function (): void {
+
+    });
+
+    it('allows creating appointment when the doctor work at the selected clinic', function (): void {
+        
+    });
+});
