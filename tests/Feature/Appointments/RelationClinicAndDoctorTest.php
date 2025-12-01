9ef69600953f49f3f37ef045729604ece2585b2a
Mauro Aires <mauroaires@MacBook-Air-de-Mauro.local>
2025-12-01 15:03:49 -0300

    refactor: Corrections PhpStan

--- tests/Feature/Appointments/RelationClinicAndDoctorTest.php
+++ tests/Feature/Appointments/RelationClinicAndDoctorTest.php
@@ -6,10 +6,8 @@ namespace Tests\Feature\Appointments;
 
 describe('clinic-doctor relation validation', function (): void {
     it('throw an exception if the doctor does not work at the selected clinic', function (): void {
-
     });
 
     it('allows creating appointment when the doctor work at the selected clinic', function (): void {
-        
     });
 });
