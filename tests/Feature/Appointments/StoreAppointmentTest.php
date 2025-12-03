07851e743272255ff23f80ff28d3b1234af76231
Mauro Aires <mauroaires@MacBook-Air-de-Mauro.local>
2025-12-03 15:01:57 -0300

    refactor: Migrate appointment tests to API responses

--- tests/Feature/Appointments/StoreAppointmentTest.php
+++ tests/Feature/Appointments/StoreAppointmentTest.php
@@ -9,110 +9,109 @@ use Database\Factories\AppointmentFactory;
 use Database\Factories\ClinicFactory;
 use Database\Factories\DoctorFactory;
 use Database\Factories\UserFactory;
-use Lightit\Appointments\App\Exceptions\InvalidDatesException;
-use Lightit\Appointments\App\Exceptions\OverlappingException;
 use Lightit\Appointments\App\Exceptions\RelationException;
 use Lightit\Appointments\Domain\Actions\UpsertAppointmentAction;
 use Lightit\Appointments\Domain\Actions\ValidateClinicDoctorRelation;
 use Lightit\Appointments\Domain\Actions\ValidateDoctorOverlapping;
 use Lightit\Appointments\Domain\Actions\ValidateUserOverlapping;
 use Lightit\Appointments\Domain\DataTransferObjects\AppointmentDto;
-use Lightit\Appointments\Domain\Models\Appointment;
-use Lightit\Doctors\Domain\Models\Doctor;
 use Tests\RequestFactories\StoreAppointmentRequestFactory;
 
-use function Pest\Laravel\assertDatabaseHas;
+use function Pest\Laravel\actingAs;
 use function Pest\Laravel\postJson;
 
 describe('appointments', function (): void {
-    it('throw an exception if the doctor already has an appointment', function (): void {
-        $appointment = StoreAppointmentRequestFactory::new()->create();
-
-        $existingAppointment = AppointmentFactory::new()->create([
-            'doctor_id' => DoctorFactory::new()->createOne(),
-            'clinic_id' => ClinicFactory::new()->createOne(),
-            'start_time' => CarbonImmutable::now(),
-            'end_time' => CarbonImmutable::now()->addHour(),
-        ]);
-
-        $response = postJson(url('api/appointments/'), $appointment);
-
-        $response->assertCreated();
-
-        assertDatabaseHas('appointments', [
-            'doctor_id' => $appointment['doctor_id'],
-            'clinic_id' => $appointment['clinic_id'],
-        ]);
-
-
-        // $dto = new AppointmentDto(
-        //     doctorId: $doctor->id,
-        //     clinicId: $clinic->id,
-        //     startTime: now()->addMinutes(30)->toDateTimeString(),
-        //     endTime: now()->addMinutes(90)->toDateTimeString(),
-        // );
-
-        // $action = new UpsertAppointmentAction(
-        //     new ValidateDoctorOverlapping(),
-        //     new ValidateUserOverlapping(),
-        //     new ValidateClinicDoctorRelation()
-        // );
-
-        // try {
-        //     $action->execute($dto, $user);
-        //     $this->fail('Expected OverlappingException was not thrown.');
-        // } catch (OverlappingException $e) {
-        //     expect($e->getMessage())->toBe('This doctor has an appointment scheduled at this time.');
-        // }
-    });
-
-    it('throw a InvalidDatesException if the end_time is a date after start_time', function (): void {
-        $doctor = DoctorFactory::new()->createOne();
+    it('throws an exception if the doctor already has an appointment', function (): void {
         $user = UserFactory::new()->createOne();
+        actingAs($user);
+        $doctor = DoctorFactory::new()->createOne();
         $clinic = ClinicFactory::new()->createOne();
         $clinic->doctors()->attach($doctor);
 
-        $dto = new AppointmentDto(
-            doctorId: $doctor->id,
-            clinicId: $clinic->id,
-            startTime: now()->toDateTimeString(),
-            endTime: now()->subHour()->toDateTimeString()
-        );
+        AppointmentFactory::new()->create([
+            'doctor_id' => $doctor->id,
+            'clinic_id' => $clinic->id,
+            'start_time' => now(),
+            'end_time' => CarbonImmutable::now()->addHour(),
+        ]);
+        $appointment = StoreAppointmentRequestFactory::new()->create([
+            'doctorId' => $doctor->id,
+            'clinicId' => $clinic->id,
+            'startTime' => CarbonImmutable::now()->addMinutes(5),
+            'endTime' => CarbonImmutable::now()->addHour(),
+        ]);
 
-        $action = new UpsertAppointmentAction(
-            new ValidateDoctorOverlapping(),
-            new ValidateUserOverlapping(),
-            new ValidateClinicDoctorRelation(),
-        );
+        $response = postJson('/api/appointments', $appointment);
 
-        try {
-            $action->execute($dto, $user);
-            $this->fail('Expected error message was not thrown.');
-        } catch (InvalidDatesException $e) {
-            expect($e->getMessage())->tobe('The end time field must be a date after start time.');
-        }
+        $response->assertStatus(409);
+        $response->assertJson([
+            'error' => [
+                'code' => 'APPOINTMENT_OVERLAP',
+                'message' => 'This doctor has an appointment scheduled at this time.',
+            ],
+        ]);
+    });
+
+    it('throw a InvalidDatesException if the end time date is before start time', function (): void {
+        $user = UserFactory::new()->createOne();
+        actingAs($user);
+        $doctor = DoctorFactory::new()->createOne();
+        $clinic = ClinicFactory::new()->createOne();
+        $clinic->doctors()->attach($doctor);
+
+        $appointment = StoreAppointmentRequestFactory::new()->create([
+            'doctorId' => $doctor->id,
+            'clinicId' => $clinic->id,
+            'startTime' => CarbonImmutable::now()->toDateTimeString(),
+            'endTime' => CarbonImmutable::now()->subHour()->toDateTimeString(),
+        ]);
+
+        $response = postJson('/api/appointments', $appointment);
+
+        $response->assertStatus(422);
+
+        $response->assertJson([
+            'error' => [
+                'code' => 'validation_failed',
+                'message' => 'The end time field must be a date after start time.',
+                'fields' => [
+                    'endTime' => [
+                        'The end time field must be a date after start time.',
+                    ],
+                ],
+            ],
+        ]);
     });
 
     it('does not throw a InvalidDatesException if the end time date is after to start time', function (): void {
-        $doctor = DoctorFactory::new()->createOne();
         $user = UserFactory::new()->createOne();
+        actingAs($user);
+        $doctor = DoctorFactory::new()->createOne();
         $clinic = ClinicFactory::new()->createOne();
         $clinic->doctors()->attach($doctor);
 
-        $dto = new AppointmentDto(
-            doctorId: $doctor->id,
-            clinicId: $clinic->id,
-            startTime: now()->toDateTimeString(),
-            endTime: now()->addHour()->toDateTimeString()
-        );
 
-        $action = new UpsertAppointmentAction(
-            new ValidateDoctorOverlapping(),
-            new ValidateUserOverlapping(),
-            new ValidateClinicDoctorRelation(),
-        );
+        $appointment = StoreAppointmentRequestFactory::new()->create([
+            'doctorId' => $doctor->id,
+            'clinicId' => $clinic->id,
+            'startTime' => CarbonImmutable::now()->addMinute()->toDateTimeString(),
+            'endTime' => CarbonImmutable::now()->addHour()->toDateTimeString(),
+        ]);
 
-        expect(fn (): Appointment => $action->execute($dto, $user))->not()->toThrow(InvalidDatesException::class);
+        $response = postJson('/api/appointments', $appointment);
+
+        $response->assertStatus(201);
+
+        $response->assertJsonStructure([
+            'data' => [
+                'id',
+                'doctor_id',
+                'user_id',
+                'clinic_id',
+                'start_time',
+                'end_time',
+            ],
+        ]);
     });
 
     it('throw an exception if the doctor does not work at the selected clinic', function (): void {
@@ -123,8 +122,8 @@ describe('appointments', function (): void {
         $dto = new AppointmentDto(
             doctorId: $doctor->id,
             clinicId: $clinic->id,
-            startTime: now()->toDateTimeString(),
-            endTime: now()->addHour()->toDateTimeString()
+            startTime: CarbonImmutable::now()->toDateTimeString(),
+            endTime: CarbonImmutable::now()->addHour()->toDateTimeString()
         );
 
         $action = new UpsertAppointmentAction(
@@ -150,8 +149,8 @@ describe('appointments', function (): void {
         $dto = new AppointmentDto(
             doctorId: $doctor->id,
             clinicId: $clinic->id,
-            startTime: now()->toDateTimeString(),
-            endTime: now()->addHour()->toDateTimeString()
+            startTime: CarbonImmutable::now()->toDateTimeString(),
+            endTime: CarbonImmutable::now()->addHour()->toDateTimeString()
         );
 
         $action = new UpsertAppointmentAction(
@@ -163,32 +162,5 @@ describe('appointments', function (): void {
         expect(fn (): \Lightit\Appointments\Domain\Models\Appointment => $action->execute($dto, $user))->not()->toThrow(
             RelationException::class
         );
-    });
-
-    it('throw a InvalidDatesException if the start time date is before to the current date', function (): void {
-    });
-
-    it('creates an appointment if all rules pass', function (): void {
-        $doctor = DoctorFactory::new()->createOne();
-        $user = UserFactory::new()->createOne();
-        $clinic = ClinicFactory::new()->createOne();
-        $clinic->doctors()->attach($doctor);
-
-        $dto = new AppointmentDto(
-            doctorId: $doctor->id,
-            clinicId: $clinic->id,
-            startTime: now()->toDateTimeString(),
-            endTime: now()->addHour()->toDateTimeString()
-        );
-
-        $action = new UpsertAppointmentAction(
-            new ValidateDoctorOverlapping(),
-            new ValidateUserOverlapping(),
-            new ValidateClinicDoctorRelation()
-        );
-
-        $appointment = $action->execute(appointmentDto: $dto, user: $user);
-
-        assertDatabaseHas('appointments', ['id' => $appointment->id]);
     });
 });
